5b412de58812d312ecfce30b6d2889459d31c261
Julien Dubois <julien.dubois@gmail.com>
2024-02-12 19:39:40 +0000

    Remove unused import statement

--- src/main/java/com/example/demo/WeatherController.java
+++ src/main/java/com/example/demo/WeatherController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
